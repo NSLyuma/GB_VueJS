90e13e0931d410ff48741889ebfc62b3cce1ae19
NSLyuma <iinokayo@mail.ru>
2021-05-06 20:50:40 +0300

    Homework for lesson_1 addition

--- gbvueproj/src/components/Calc.vue
+++ gbvueproj/src/components/Calc.vue
@@ -29,26 +29,55 @@ export default {
   methods: {
     calculate(operation) {
       //   console.log(operation.target.outerText);
-      switch (operation.target.outerText) {
-        case "+":
-          this.result = this.operand1 + this.operand2;
-          break;
-        case "-":
-          this.result = this.operand1 - this.operand2;
-          break;
-        case "*":
-          this.result = this.operand1 * this.operand2;
-          break;
-        case "/":
-          this.result = this.operand1 / this.operand2;
-          break;
-        case "^":
-          this.result = Math.pow(this.operand1, this.operand2);
-          break;
-        case "f":
-          this.result = Math.floor(this.operand1 / this.operand2);
-          break;
-      }
+      //   switch (operation.target.outerText) {
+      //     case "+":
+      //       this.result = this.operand1 + this.operand2;
+      //       break;
+      //     case "-":
+      //       this.result = this.operand1 - this.operand2;
+      //       break;
+      //     case "*":
+      //       this.result = this.operand1 * this.operand2;
+      //       break;
+      //     case "/":
+      //       this.result = this.operand1 / this.operand2;
+      //       break;
+      //     case "^":
+      //       this.result = Math.pow(this.operand1, this.operand2);
+      //       break;
+      //     case "f":
+      //       this.result = Math.floor(this.operand1 / this.operand2);
+      //       break;
+      //   }
+
+      let calc = {
+        "+": (op1, op2) => {
+          return op1 + op2;
+        },
+        "-": (op1, op2) => {
+          return op1 - op2;
+        },
+        "*": (op1, op2) => {
+          return op1 * op2;
+        },
+        "/": (op1, op2) => {
+          return op1 / op2;
+        },
+        "^": (op1, op2) => {
+          return Math.pow(op1, op2);
+        },
+        f: (op1, op2) => {
+          return Math.floor(op1 / op2);
+        },
+        operation: (op1, op2, operator) => {
+          return calc[operator](op1, op2);
+        },
+      };
+      this.result = calc.operation(
+        this.operand1,
+        this.operand2,
+        operation.target.outerText
+      );
     },
   },
 };
