0dc1b0c67dc63f98d970aa7c33f2d794d64d48ef
NSLyuma <iinokayo@mail.ru>
2021-05-17 17:36:01 +0300

    Started homework for lesson 2

--- gbvueproj/src/components/Calc.vue
+++ gbvueproj/src/components/Calc.vue
@@ -4,14 +4,64 @@
     <input v-model.number="operand1" />
     <input v-model.number="operand2" />
     <div>
-      <button @click="calculate($event)">+</button>
-      <button @click="calculate($event)">-</button>
-      <button @click="calculate($event)">*</button>
-      <button @click="calculate($event)">/</button>
-      <button @click="calculate($event)">^</button>
-      <button @click="calculate($event)">f</button>
+      <button v-for="op in operations" :key="op" @click="calculate(op)">
+        {{ op }}
+      </button>
+      <!-- эти 6 строк можно заменить одной, которая сверху
+      <button @click="calculate('+')">+</button>
+      <button @click="calculate('-')">-</button>
+      <button @click="calculate('*')">*</button>
+      <button @click="calculate('/')">/</button>
+      <button @click="calculate('^')">^</button>
+      <button @click="calculate('f')">f</button> -->
     </div>
     <p>{{ result }}</p>
+
+    <div class="error" v-if="error">
+      <!-- если выполняется условие (error не пустой), то покажи этот блок -->
+      <!-- v-if полностью убирает блок из вёрстки, v-show скрывает с помощью display: none -->
+      {{ error }}
+    </div>
+
+    <!-- пример работы с if
+    <div v-if="result < 0">Получилось отрицательное число</div>
+    <div v-else-if="result <= 1000">
+      Результат находится в промежутке от 0 до 1000
+    </div>
+    <div v-else>Результат слишком большой (>1000)</div> -->
+
+    <div>
+      <label>
+        <input type="checkbox" id="checkbox" v-model="checked" />
+        Отобразить экранную клавиатуру
+      </label>
+    </div>
+
+    <div v-if="checked">
+      <button v-for="(item, index) in numbers" v-bind:key="index">
+        {{ item }}
+      </button>
+
+      <div>
+        <input type="radio" name="operand" id="first" v-model="first" checked />
+        <label for="first">Операнд 1</label>
+        <input type="radio" name="operand" id="second" v-model="second" />
+        <label for="second">Операнд 2</label>
+      </div>
+    </div>
+    <br />
+    <br />
+    <br />
+
+    <div>
+      <!-- <input type="radio" id="one" value="Один" v-model="picked" />
+      <label for="one">Один</label>
+      <br />
+      <input type="radio" id="two" value="Два" v-model="picked" />
+      <label for="two">Два</label>
+      <br />
+      <span>Выбрано: {{ picked }}</span> -->
+    </div>
   </div>
 </template>
 
@@ -19,9 +69,15 @@
 export default {
   name: "Calc",
   data: () => ({
+    operations: ["+", "-", "*", "/", "^", "f"],
     operand1: 0,
     operand2: 0,
     result: 0,
+    error: "",
+    numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    checked: true,
+    first: true,
+    second: false,
   }),
   props: {
     title: String,
@@ -29,7 +85,7 @@ export default {
   methods: {
     calculate(operation) {
       //   console.log(operation.target.outerText);
-      //   switch (operation.target.outerText) {
+      //   switch (operation) {
       //     case "+":
       //       this.result = this.operand1 + this.operand2;
       //       break;
@@ -49,6 +105,12 @@ export default {
       //       this.result = Math.floor(this.operand1 / this.operand2);
       //       break;
       //   }
+      const { operand1, operand2 } = this;
+
+      if (operation === "/" && operand2 === 0) {
+        this.error = "Division by zero!";
+        return;
+      }
 
       let calc = {
         "+": (op1, op2) => {
@@ -73,12 +135,18 @@ export default {
           return calc[operator](op1, op2);
         },
       };
-      this.result = calc.operation(
-        this.operand1,
-        this.operand2,
-        operation.target.outerText
-      );
+      this.result = calc.operation(operand1, operand2, operation);
     },
+  },
+  computed: {
+    //не принимают параметры и обязательно возвращают какое-то значение
   },
 };
 </script>
+
+<style scoped lang="scss">
+.error {
+  padding: 20px;
+  border: 1px solid red;
+}
+</style>
